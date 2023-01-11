11df88f07f3b5032058b98fa952e0b5d07d4acbe
rhuanbello <rhuambello@gmail.com>
2023-01-11 20:05:07 -0300

    Section 02: Pagination, Pre-fetching and Mutations: (16~20)

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 
 async function fetchComments(postId) {
   const response = await fetch(
@@ -24,10 +24,17 @@ async function updatePost(postId) {
 }
 
 export function PostDetail({ post }) {
-  const { data, isLoading, isError, error } = useQuery(['comments', post.id], () => fetchComments(post.id), {
-    staleTime: 2000,
-    cacheTime: 1000
-  })
+  const { data, isLoading, isError, error } = useQuery(
+    ['comments', post.id],
+    () => fetchComments(post.id),
+    {
+      staleTime: 2000,
+      cacheTime: 1000
+    }
+  );
+
+  const deleteMutation = useMutation((postId) => deletePost(postId));
+  const updateMutation = useMutation((postId) => updatePost(postId))
 
   if (isLoading) return <h3>Loading...</h3>
 
@@ -37,12 +44,38 @@ export function PostDetail({ post }) {
       <p>{error.toString()}</p>
     </>
   )
-    
+
+  const renderMutationsMessages = () => {
+    if (deleteMutation.isError) {
+      return <p style={{ color: "red" }}>Error deleting post</p>;
+    } else if (deleteMutation.isLoading) {
+      return <p style={{ color: "red" }}>Deleting post...</p>;
+    } else if (deleteMutation.isSuccess) {
+      return <p style={{ color: "green" }}>Post deleted successfully</p>;
+    }
+
+    if (updateMutation.isError) {
+      return <p style={{ color: "red" }}>Error updating post</p>;
+    } else if (updateMutation.isLoading) {
+      return <p style={{ color: "red" }}>Updating post...</p>;
+    } else if (updateMutation.isSuccess) {
+      return <p style={{ color: "green" }}>Post updated successfully</p>;
+    }
+  }
 
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+
+      <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+
+      {renderMutationsMessages()}
+
+      <button
+        onClick={() => updateMutation.mutate(post.id)}
+      >
+        Update title
+      </button>
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
