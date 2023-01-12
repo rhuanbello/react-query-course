d078b3784305868649ff2e5876ae436c8108d821
rhuanbello <rhuambello@gmail.com>
2023-01-11 21:00:55 -0300

    Section 03: Infinite Queries for Loading Data "Just in Time"

--- base-infinite-swapi/src/species/InfiniteSpecies.jsx
+++ base-infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -1,4 +1,6 @@
 import InfiniteScroll from "react-infinite-scroller";
+import { useInfiniteQuery } from "react-query";
+
 import { Species } from "./Species";
 
 const initialUrl = "https://swapi.dev/api/species/";
@@ -8,6 +10,49 @@ const fetchUrl = async (url) => {
 };
 
 export function InfiniteSpecies() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    isFetching,
+    isError,
+    error,
+  } = useInfiniteQuery(
+    "sw-species",
+    ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    {
+      getNextPageParam: (lastPage) => lastPage.next || undefined,
+    }
+  );
+
+  if (isLoading) return <div className="loading">Loading...</div>;
+  if (isError) return <div>Error! {error.toString()}</div>;
+
+  return (
+    <>
+      {isFetching && <div className="loading">Loading...</div>}
+      <InfiniteScroll
+        // add initialLoad={false} to prevent loading two pages on
+        // component mount (see
+        // https://www.udemy.com/course/learn-react-query/learn/#questions/18222646/)
+        initialLoad={false}
+        loadMore={fetchNextPage}
+        hasMore={hasNextPage}
+      >
+        {data.pages.map((pageData) => {
+          return pageData.results.map((species) => {
+            return (
+              <Species
+                key={species.name}
+                name={species.name}
+                language={species.language}
+                averageLifespan={species.average_lifespan}
+              />
+            );
+          });
+        })}
+      </InfiniteScroll>
+    </>
+  );
 }
